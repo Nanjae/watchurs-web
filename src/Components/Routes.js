70cacb1e2258163ad53c0b821ab7dd13b3fa3458
Nanjae <33223057+Nanjae@users.noreply.github.com>
2019-12-02 16:55:15 +0900

    fix riot.txt route

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Home from "../Routes/League/LeagueRank/LeagueRank";
-import Riot from "../Routes/Riot";
+// import Riot from "../Routes/Riot";
 
 const DefaultRoute = () => (
   <Switch>
-    <Route path="/riot.txt" component={Riot} />
+    {/* <Route path="/riot.txt" component={Riot} /> */}
     <Route path="/" component={Home} />
     {/* <Redirect from="*" to="/" /> */}
   </Switch>
