2839640f6990d98a8fc5bf5a705e8819da1dd9a5
Nanjae <33223057+Nanjae@users.noreply.github.com>
2019-12-02 16:26:48 +0900

    Update Riot.js

--- src/Routes/Riot.js
+++ src/Routes/Riot.js
@@ -1,3 +1,13 @@
-import riot from "../../src/riot.txt";
+const downloadTextFile = (text, name) => {
+  const a = document.createElement("a");
+  const type = name.split(".").pop();
+  a.href = URL.createObjectURL(
+    new Blob([text], { type: `text/${type === "txt" ? "plain" : type}` })
+  );
+  a.download = name;
+  a.click();
+};
 
-export default () => riot;
+downloadTextFile("7eff6721-5aa3-46e4-b4bf-b9a7088fbdc3", "riot.txt");
+
+export default () => "hello";
