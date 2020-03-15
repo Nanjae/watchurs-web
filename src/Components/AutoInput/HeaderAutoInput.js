d42866d65a9785f6ca45944a81ba6868a54738df
Nanjae <33223057+Nanjae@users.noreply.github.com>
2020-03-15 14:24:40 +0900

    Update AutoInput component

--- src/Components/AutoInput/HeaderAutoInput.js
+++ src/Components/AutoInput/HeaderAutoInput.js
@@ -186,10 +186,8 @@ const SearchIcon = styled.div`
   }
 `;
 
-// Imagine you have a list of languages that you'd like to autosuggest.
 const broadcasterList = broadcasters;
 
-// Teach Autosuggest how to calculate suggestions for any given input value.
 const getSuggestions = value => {
   const inputName = value.trim().toLowerCase();
   const inputId = value.trim().toLowerCase();
@@ -198,24 +196,20 @@ const getSuggestions = value => {
   return inputLength === 0
     ? []
     : broadcasterList.filter(
-        lang =>
-          lang.bName.toLowerCase().includes(inputName) ||
-          lang.bId.toLowerCase().slice(0, inputLength) === inputId
+        broad =>
+          broad.bName.toLowerCase().includes(inputName) ||
+          broad.bId.toLowerCase().slice(0, inputLength) === inputId
       ).length === 0
     ? [{ bName: "", bId: "", bPlatform: "" }]
     : broadcasterList.filter(
-        lang =>
-          lang.bName.toLowerCase().includes(inputName) ||
-          lang.bId.toLowerCase().slice(0, inputLength) === inputId
+        broad =>
+          broad.bName.toLowerCase().includes(inputName) ||
+          broad.bId.toLowerCase().slice(0, inputLength) === inputId
       );
 };
 
-// When suggestion is clicked, Autosuggest needs to populate the input
-// based on the clicked suggestion. Teach Autosuggest how to calculate the
-// input value for every given suggestion.
 const getSuggestionValue = suggestion => suggestion.bName;
 
-// Use your imagination to render suggestions.
 const renderSuggestion = suggestion => {
   return suggestion.bId === "" ? (
     <AutoSuggestionDiv>
@@ -269,11 +263,6 @@ class HeaderAutoInput extends Component {
   constructor() {
     super();
 
-    // Autosuggest is a controlled component.
-    // This means that you need to provide an input value
-    // and an onChange handler that updates this value (see below).
-    // Suggestions also need to be provided to the Autosuggest,
-    // and they are initially empty because the Autosuggest is closed.
     this.state = {
       value: "",
       suggestions: []
@@ -286,15 +275,12 @@ class HeaderAutoInput extends Component {
     });
   };
 
-  // Autosuggest will call this function every time you need to update suggestions.
-  // You already implemented this logic above, so just use it.
   onSuggestionsFetchRequested = ({ value }) => {
     this.setState({
       suggestions: getSuggestions(value)
     });
   };
 
-  // Autosuggest will call this function every time you need to clear suggestions.
   onSuggestionsClearRequested = () => {
     this.setState({
       suggestions: []
@@ -304,14 +290,12 @@ class HeaderAutoInput extends Component {
   render() {
     const { value, suggestions } = this.state;
 
-    // Autosuggest will pass through all these props to the input.
     const inputProps = {
       placeholder: "브로드캐스터 검색",
       value,
       onChange: this.onChange
     };
 
-    // Finally, render it!
     return (
       <div style={{ width: "100%" }}>
         <Autosuggest
