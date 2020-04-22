8a7ba66d90d910f313b3d7867d194bc108203c57
Nanjae <33223057+Nanjae@users.noreply.github.com>
2020-04-22 16:40:15 +0900

    Create base form

--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -1,19 +1,12 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
-// import AtomyLight from "./fonts/AtomyLight.ttf";
-// import AtomyMedium from "./fonts/AtomyMedium.ttf";
-// import AtomyBold from "./fonts/AtomyBold.ttf";
-// import SpoqaHanSansThin from "./fonts/SpoqaHanSansThin.ttf";
-// import SpoqaHanSansLight from "./fonts/SpoqaHanSansLight.ttf";
-// import SpoqaHanSansRegular from "./fonts/SpoqaHanSansRegular.ttf";
-// import SpoqaHanSansBold from "./fonts/SpoqaHanSansBold.ttf";
 
 export default createGlobalStyle`
     ${reset};
     @import url("https://fonts.googleapis.com/css?family=Nanum+Gothic&display=swap");
     @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
     body {
-        color: ${props => props.theme.darkFont};
+        color: ${(props) => props.theme.darkFont};
         font-family: 'Noto Sans KR', sans-serif;
         font-weight: 300;
         font-size: 14px;
@@ -21,7 +14,7 @@ export default createGlobalStyle`
     a {
         font-family: 'Noto Sans KR', sans-serif;
         font-weight: 300;
-        color: ${props => props.theme.darkFont};
+        color: ${(props) => props.theme.darkFont};
         text-decoration: none;
     }
     /* input {
