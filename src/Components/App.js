831b43d068d6f3e51630192dc40b6d76d5bd6370
Nanjae <33223057+Nanjae@users.noreply.github.com>
2020-04-23 17:16:21 +0900

    Clean up master branch

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,18 +4,19 @@ import styled, { ThemeProvider } from "styled-components";
 import Theme from "../Styles/Theme";
 import { BrowserRouter as Router } from "react-router-dom";
 import { DefaultRoute } from "./Routes";
+import useWindowDimensions from "../Hooks/useWindowDimensions";
 
 const Wrapper = styled.div`
-  display: flex;
-  justify-content: center;
+  overflow: hidden;
 `;
 
 export default () => {
+  const { windowWidth, windowHeight } = useWindowDimensions();
   return (
     <ThemeProvider theme={Theme}>
       <GlobalStyles />
       <Router>
-        <Wrapper>
+        <Wrapper style={{ width: windowWidth, height: windowHeight }}>
           <DefaultRoute />
         </Wrapper>
       </Router>
--- src/Routes/Home/Home.js
+++ src/Routes/Home/Home.js
@@ -1,167 +1,127 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
-import Intro from "../../Components/Contents/Intro";
-import ExplorerService from "../../Components/Contents/ExplorerService";
-import TechStack from "../../Components/Contents/TechStack";
-import Footer from "../../Components/Footer";
 import useWindowDimensions from "../../Hooks/useWindowDimensions";
-import Header from "../../Components/Header";
+import bg_lol from "../../Assets/Images/Bg/bg_lol.jpg";
 
 const Wrapper = styled.div`
-  height: fit-content;
-  width: 100%;
-  max-width: 1780px;
-  background-color: ${(props) => props.theme.white};
+  background-color: white;
   display: flex;
-  flex-direction: column;
-  align-items: center;
   justify-content: center;
-  box-shadow: 0px 0px 2rem rgba(0, 0, 0, 0.1);
-  user-select: none;
+  align-items: center;
+  background-color: darkgray;
 `;
 
-// Ref Point
+const ControlDiv = styled.div`
+  position: absolute;
+  width: 50%;
+  height: 20%;
+  background-color: midnightblue;
+  border-radius: 20px;
+  box-shadow: 5px 5px 0px 0px mediumblue;
+  /* border: 2px solid violet; */
+`;
 
-const IntroRefPoint = styled.div``;
-const ServicesRefPoint = styled.div``;
-const TechStackRefPoint = styled.div``;
-
-// Explorer Services 타이틀
-
-const ServiceTitleDiv = styled.div`
-  width: 100%;
-  height: fit-content;
+const ContentDiv = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  @media screen and (max-width: 599px) {
-    margin-top: 40px;
-    margin-bottom: 20px;
-  }
-  @media screen and (min-width: 600px) {
-    margin-top: 40px;
-    margin-bottom: 30px;
-  }
-  @media screen and (min-width: 800px) {
-    margin-top: 60px;
-    margin-bottom: 40px;
-  }
-  @media screen and (min-width: 1300px) {
-    margin-top: 80px;
-    margin-bottom: 60px;
-  }
-`;
-
-const ServiceTitleMainText = styled.div`
-  font-size: 24px;
-  font-weight: 400;
-  margin-bottom: 20px;
-`;
-
-const ServiceTitleSubText = styled.div`
-  font-size: 18px;
-  font-weight: 300;
-  color: ${(props) => props.theme.darkFontH};
-`;
-
-// 기술 스택 타이틀
-
-const TechStackTitleDiv = styled.div`
   width: 100%;
-  height: fit-content;
+  height: 100%;
+`;
+
+const ContentGrid = styled.div`
+  display: grid;
+  width: 100%;
+  height: 100%;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 20px;
+`;
+
+const ContentBox = styled.div`
+  width: 100%;
+  height: 100%;
   display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.lightGray};
-  @media screen and (max-width: 599px) {
-    padding-top: 40px;
-  }
-  @media screen and (min-width: 600px) {
-    padding-top: 40px;
-  }
-  @media screen and (min-width: 800px) {
-    padding-top: 60px;
-  }
-  @media screen and (min-width: 1300px) {
-    padding-top: 80px;
-  }
 `;
 
-const TechStackMainText = styled.div`
-  font-size: 24px;
-  font-weight: 400;
-  margin-bottom: 20px;
+const ContentInner = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
 `;
 
-const TechStackSubText = styled.div`
-  font-size: 18px;
-  font-weight: 300;
-  color: ${(props) => props.theme.darkFontH};
+const ContentBg = styled.div`
+  background-image: url(${(props) => props.url});
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+  background-position: center center;
+  border: 2px solid lightgray;
+  /* box-shadow: 0px 0px 1px 1px darkred; */
 `;
-
-const scrollToRef = (ref, offTop = 76) =>
-  window.scrollTo(0, ref.current.offsetTop - offTop);
-
-const useMountEffect = (fun) => useEffect(fun, []);
 
 export default () => {
-  const { windowWidth } = useWindowDimensions();
-
-  const introRef = useRef(null);
-  const servicesRef = useRef(null);
-  const techStackRef = useRef(null);
-
-  useMountEffect(
-    () => scrollToRef(introRef),
-    () => scrollToRef(servicesRef),
-    () => scrollToRef(techStackRef),
-    []
-  );
+  const { windowWidth, windowHeight } = useWindowDimensions();
 
   return (
     <>
-      <Header
-        introScroll={() => scrollToRef(introRef, windowWidth < 600 ? 120 : 76)}
-        servicesScroll={() =>
-          scrollToRef(servicesRef, windowWidth < 600 ? 120 : 76)
-        }
-        techStackScroll={() =>
-          scrollToRef(techStackRef, windowWidth < 600 ? 120 : 76)
-        }
-      />
-      <Wrapper>
-        <IntroRefPoint ref={introRef} />
-        <Intro />
-        <ServicesRefPoint ref={servicesRef} />
-        <ServiceTitleDiv>
-          <ServiceTitleMainText>Explore Services</ServiceTitleMainText>
-          <ServiceTitleSubText>
-            당신을 위한 서비스는 계속 추가됩니다.
-          </ServiceTitleSubText>
-        </ServiceTitleDiv>
-        <ExplorerService />
-        <TechStackRefPoint ref={techStackRef} />
-        <TechStackTitleDiv>
-          <TechStackMainText>Tech Stack</TechStackMainText>
-          <TechStackSubText>
-            와쳐스에서 사용된 기술 스택입니다.
-          </TechStackSubText>
-        </TechStackTitleDiv>
-        <TechStack />
-        <Footer
-          introScroll={() =>
-            scrollToRef(introRef, windowWidth < 600 ? 120 : 76)
-          }
-          servicesScroll={() =>
-            scrollToRef(servicesRef, windowWidth < 600 ? 120 : 76)
-          }
-          techStackScroll={() =>
-            scrollToRef(techStackRef, windowWidth < 600 ? 120 : 76)
-          }
-        />
+      <Wrapper style={{ width: "100%", height: "100%" }}>
+        {/* <ControlDiv></ControlDiv> */}
+        <ContentDiv>
+          <ContentGrid>
+            <ContentBox>
+              <ContentInner
+                style={{
+                  justifyContent: "flex-start",
+                  alignItems: "flex-start",
+                }}
+              >
+                <ContentBg url={bg_lol} />
+              </ContentInner>
+            </ContentBox>
+            <ContentBox>
+              <ContentInner
+                style={{
+                  justifyContent: "flex-end",
+                  alignItems: "flex-start",
+                }}
+              >
+                <ContentBg url={bg_lol} />
+              </ContentInner>
+            </ContentBox>
+            <ContentBox>
+              <ContentInner
+                style={{
+                  justifyContent: "flex-start",
+                  alignItems: "flex-end",
+                }}
+              >
+                <ContentBg url={bg_lol} />
+              </ContentInner>
+            </ContentBox>
+            <ContentBox>
+              <ContentInner
+                style={{
+                  justifyContent: "flex-end",
+                  alignItems: "flex-end",
+                }}
+              >
+                <ContentBg url={bg_lol} />
+              </ContentInner>
+            </ContentBox>
+          </ContentGrid>
+          {/* <ContentGrid>
+            
+          </ContentGrid> */}
+        </ContentDiv>
       </Wrapper>
+      <Wrapper
+        style={{
+          width: windowWidth,
+          height: windowHeight,
+          backgroundColor: "yellow",
+        }}
+      ></Wrapper>
     </>
   );
 };
