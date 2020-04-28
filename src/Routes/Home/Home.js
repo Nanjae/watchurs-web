5e85eaa0037dce6a6f3ac5415753e509d7952ab4
Nanjae <33223057+Nanjae@users.noreply.github.com>
2020-04-28 17:46:35 +0900

    Update Home route

--- src/Routes/Home/Home.js
+++ src/Routes/Home/Home.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
-import styled from "styled-components";
+import styled, { keyframes, css } from "styled-components";
 import useWindowDimensions from "../../Hooks/useWindowDimensions";
-// import Theme from "../../Styles/Theme";
-import bg1 from "../../Assets/Images/Bg/bg1.jpg";
-import bg2 from "../../Assets/Images/Bg/bg2.png";
+import bg01 from "../../Assets/Images/Bg/bg01.jpg";
+import bg02 from "../../Assets/Images/Bg/bg02.jpg";
+import bg03 from "../../Assets/Images/Bg/bg03.jpg";
+import bg04 from "../../Assets/Images/Bg/bg04.jpg";
+import icon_contrast_dark from "../../Assets/Icons/icon_contrast_dark.png";
+// import icon_contrast_light from "../../Assets/Icons/icon_contrast_light.png";
 import Swiper from "react-id-swiper";
 import "swiper/css/swiper.css";
 
@@ -67,7 +70,7 @@ const IntroUnderImageOpacity = styled.div`
   width: 100%;
   height: 100%;
   background-color: ${(props) => props.theme.bgDark};
-  opacity: 0.4;
+  opacity: 0.6;
 `;
 
 const IntroUpperWrapper = styled.div`
@@ -113,7 +116,7 @@ const IntroUpperHeaderTitleText = styled.div`
   font-size: 18px;
   font-weight: 700;
   letter-spacing: 3px;
-  color: white;
+  color: ${(props) => props.theme.fontDark};
   transform: rotate(-90deg) translate(-50%, -280%);
   white-space: nowrap;
 `;
@@ -122,15 +125,87 @@ const IntroUpperHeaderMenuDiv = styled.div`
   width: 100%;
   height: 20%;
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  align-items: center;
 `;
 
-const IntroUpperHeaderMenuIcon = styled.div`
-  background-color: white;
+const IntroUpperHeaderMenuInner = styled.div`
+  width: fit-content;
+  height: fit-content;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  cursor: pointer;
+`;
+
+const IntroUpperHeaderMenuLineDiv = styled.div`
   width: 60px;
-  height: 60px;
-  margin-bottom: 10px;
+  height: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-bottom: 20px;
+`;
+
+const menuUpperLine = keyframes`
+  0% {
+    width:50%;
+    opacity: 0.4;
+  }
+  33% {
+    width:80%;
+    opacity: 1;
+  }
+  66% {
+    width:40%;
+    opacity: 0.4;
+  }
+  100% {
+    width:100%;
+    opacity: 1;
+  }
+`;
+
+const menuUnderLine = keyframes`
+  0% {
+    width:100%;
+    opacity: 1;
+  }
+  33% {
+    width:50%;
+    opacity: 0.4;
+  }
+  66% {
+    width:80%;
+    opacity: 1;
+  }
+  100% {
+    width:40%;
+    opacity: 0.4;
+  }
+`;
+
+const IntroUpperHeaderMenuUpperLine = styled.div`
+  background-color: ${(props) => props.theme.whiteDark};
+  width: 100%;
+  height: 3px;
+  animation: ${(props) =>
+    props.headerMenuEnter
+      ? css`
+          ${menuUpperLine} 1.2s infinite linear alternate
+        `
+      : null};
+`;
+
+const IntroUpperHeaderMenuUnderLine = styled.div`
+  background-color: ${(props) => props.theme.whiteDark};
+  width: 80%;
+  height: 3px;
+  animation: ${(props) =>
+    props.headerMenuEnter
+      ? css`
+          ${menuUnderLine} 1.2s infinite linear alternate
+        `
+      : null};
 `;
 
 const IntroUpperHeaderMenuTextBox = styled.div`
@@ -142,8 +217,8 @@ const IntroUpperHeaderMenuTextBox = styled.div`
 
 const IntroUpperHeaderMenuText = styled.div`
   font-size: 15px;
-  font-weight: 200;
-  color: white;
+  font-weight: 700;
+  color: ${(props) => props.theme.fontDark};
 `;
 
 const IntroUpperHeaderIconDiv = styled.div`
@@ -159,6 +234,89 @@ const IntroUpperHeaderIcon = styled.div`
   width: 20px;
   height: 20px;
   margin-top: 40px;
+`;
+
+const IntroUpperCenterDiv = styled.div`
+  width: 38%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-end;
+`;
+
+const IntroUpperCenterInner = styled.div`
+  width: 80%;
+  height: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+`;
+
+const IntroUpperCenterSwiperBarBox = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 3px;
+  height: 120px;
+`;
+
+const IntroUpperCenterSwiperEmptyBar = styled.div`
+  position: absolute;
+  background-color: ${(props) => props.theme.grayDark};
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  width: 3px;
+  height: 120px;
+  opacity: 0.6;
+`;
+
+const IntroUpperCenterSwiperFillBar = styled.div`
+  position: absolute;
+  background-color: ${(props) => props.theme.whiteDark};
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  width: 3px;
+  height: ${(props) => (props.upperTextIndex - 1) * 40}px;
+  transition: height 0.5s;
+`;
+
+const IntroUpperCenterSwiperCircle = styled.div`
+  position: absolute;
+  background-color: ${(props) => props.theme.whiteDark};
+  width: 11px;
+  height: 11px;
+  border-radius: 50%;
+  margin-top: ${(props) => (props.upperTextIndex - 1) * 40 - 5}px;
+  transition: margin-top 0.5s;
+`;
+
+const IntroUpperCenterSwiperTextBox = styled.div`
+  display: flex;
+  align-items: center;
+  transform: rotate(-90deg) translate(-60px, -65px);
+`;
+
+const IntroUpperCenterSwiperMaxText = styled.div`
+  font-size: 20px;
+  font-weight: 500;
+  color: ${(props) => props.theme.grayDark};
+  opacity: 0.6;
+`;
+
+const IntroUpperCenterSwiperTextDivsion = styled.div`
+  font-size: 20px;
+  font-weight: 700;
+  margin: 0px 10px;
+  color: ${(props) => props.theme.grayDark};
+  opacity: 0.6;
+`;
+
+const IntroUpperCenterSwiperCurrentText = styled.div`
+  font-size: 50px;
+  font-weight: 700;
+  margin: 0px 10px;
+  color: ${(props) => props.theme.whiteDark};
 `;
 
 const IntroUpperRightDiv = styled.div`
@@ -193,7 +351,10 @@ const IntroMainContentIconDiv = styled.div`
 `;
 
 const IntroMainContentIcon = styled.div`
-  background-color: white;
+  background-image: url(${(props) => props.url});
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
   width: 20px;
   height: 20px;
   margin-left: 20px;
@@ -226,22 +387,27 @@ const IntroMainContentTextMainBox = styled.div`
 `;
 
 const IntroMainContentTextSubBox = styled.div`
+  width: fit-content;
   display: flex;
   align-items: flex-end;
   margin-top: 40px;
+  cursor: pointer;
 `;
 
 const IntroMainContentTextSubBoxText = styled.div`
   font-size: 18px;
   font-weight: 400;
+  color: ${(props) => props.theme.whiteDark};
+  transition: color 0.3s;
 `;
 
 const IntroMainContentTextSubBoxLine = styled.div`
   width: 70px;
   height: 2px;
-  margin-left: 10px;
+  margin-left: 20px;
   margin-bottom: 5px;
-  background-color: white;
+  background-color: ${(props) => props.theme.whiteDark};
+  transition: width 0.3s, margin-left 0.3s, background-color 0.3s;
 `;
 
 const IntroMainSwiperDiv = styled.div`
@@ -251,21 +417,40 @@ const IntroMainSwiperDiv = styled.div`
   justify-content: flex-end;
 `;
 
-const IntroMainSwiperBox = styled.div`
+const IntroMainSwiperInner = styled.div`
   width: 85%;
   height: fit-content;
 `;
 
-const SwiperImage = styled.div`
+const IntroMainSwiperImageBox = styled.div`
+  display: flex;
+  width: 100%;
+  height: fit-content;
+`;
+
+const IntroMainSwiperImage = styled.div`
   background-image: url(${(props) => props.url});
   background-size: cover;
   width: 280px;
   height: 180px;
 `;
 
+const IntroMainSwiperImageOpacity = styled.div`
+  position: absolute;
+  background-color: ${(props) => props.theme.bgDark};
+  width: 280px;
+  height: 180px;
+  opacity: 0;
+  transition: opacity 0.3s;
+  cursor: pointer;
+`;
+
 export default () => {
   const { windowHeight } = useWindowDimensions();
-  const [underBgIndex, setUnderBgIndex] = useState(bg1);
+  const [underBgIndex, setUnderBgIndex] = useState(bg01);
+  const [upperTextIndex, setUpperTextIndex] = useState("01");
+  const [headerMenuEnter, setHeaderMenuEnter] = useState(false);
+  const [swiperImageEnter, setSwiperImageEnter] = useState(0);
 
   return (
     <>
@@ -292,13 +477,29 @@ export default () => {
                   </IntroUpperHeaderTitleText>
                 </IntroUpperHeaderTitleDiv>
                 <IntroUpperHeaderMenuDiv>
-                  <IntroUpperHeaderMenuIcon />
-                  <IntroUpperHeaderMenuTextBox>
-                    <IntroUpperHeaderMenuText>M</IntroUpperHeaderMenuText>
-                    <IntroUpperHeaderMenuText>E</IntroUpperHeaderMenuText>
-                    <IntroUpperHeaderMenuText>N</IntroUpperHeaderMenuText>
-                    <IntroUpperHeaderMenuText>U</IntroUpperHeaderMenuText>
-                  </IntroUpperHeaderMenuTextBox>
+                  <IntroUpperHeaderMenuInner
+                    onMouseEnter={() => {
+                      setHeaderMenuEnter(true);
+                    }}
+                    onMouseLeave={() => {
+                      setHeaderMenuEnter(false);
+                    }}
+                  >
+                    <IntroUpperHeaderMenuLineDiv>
+                      <IntroUpperHeaderMenuUpperLine
+                        headerMenuEnter={headerMenuEnter}
+                      />
+                      <IntroUpperHeaderMenuUnderLine
+                        headerMenuEnter={headerMenuEnter}
+                      />
+                    </IntroUpperHeaderMenuLineDiv>
+                    <IntroUpperHeaderMenuTextBox>
+                      <IntroUpperHeaderMenuText>M</IntroUpperHeaderMenuText>
+                      <IntroUpperHeaderMenuText>E</IntroUpperHeaderMenuText>
+                      <IntroUpperHeaderMenuText>N</IntroUpperHeaderMenuText>
+                      <IntroUpperHeaderMenuText>U</IntroUpperHeaderMenuText>
+                    </IntroUpperHeaderMenuTextBox>
+                  </IntroUpperHeaderMenuInner>
                 </IntroUpperHeaderMenuDiv>
                 <IntroUpperHeaderIconDiv>
                   <IntroUpperHeaderIcon />
@@ -307,12 +508,39 @@ export default () => {
                 </IntroUpperHeaderIconDiv>
               </IntroUpperHeaderDiv>
             </IntroUpperLeftDiv>
+            <IntroUpperCenterDiv>
+              <IntroUpperCenterInner>
+                <IntroUpperCenterSwiperBarBox>
+                  <IntroUpperCenterSwiperEmptyBar />
+                  <IntroUpperCenterSwiperFillBar
+                    upperTextIndex={upperTextIndex}
+                  />
+                  <IntroUpperCenterSwiperCircle
+                    upperTextIndex={upperTextIndex}
+                  />
+                </IntroUpperCenterSwiperBarBox>
+                <IntroUpperCenterSwiperTextBox>
+                  <IntroUpperCenterSwiperCurrentText>
+                    {upperTextIndex}
+                  </IntroUpperCenterSwiperCurrentText>
+                  <IntroUpperCenterSwiperTextDivsion>
+                    /
+                  </IntroUpperCenterSwiperTextDivsion>
+                  <IntroUpperCenterSwiperMaxText>
+                    04
+                  </IntroUpperCenterSwiperMaxText>
+                </IntroUpperCenterSwiperTextBox>
+              </IntroUpperCenterInner>
+            </IntroUpperCenterDiv>
             <IntroUpperRightDiv>
               <IntroMainDiv>
                 <IntroMainContentDiv>
                   <IntroMainContentIconDiv>
                     <IntroMainContentIcon />
-                    <IntroMainContentIcon />
+                    <IntroMainContentIcon
+                      url={icon_contrast_dark}
+                      style={{ transform: "rotate(45deg)" }}
+                    />
                   </IntroMainContentIconDiv>
                   <IntroMainContentTextDiv>
                     <IntroMainContentTextMainTitle>
@@ -337,58 +565,102 @@ export default () => {
                   </IntroMainContentTextDiv>
                 </IntroMainContentDiv>
                 <IntroMainSwiperDiv>
-                  <IntroMainSwiperBox>
+                  <IntroMainSwiperInner>
                     <Swiper
                       width={280}
                       centeredSlides={true}
                       slideToClickedSlide={true}
                     >
-                      <div
+                      <IntroMainSwiperImageBox
                         style={{
                           marginLeft: "60px",
                         }}
                         onClick={() => {
-                          setUnderBgIndex(bg1);
-                          console.log("1");
+                          setUnderBgIndex(bg01);
+                          setUpperTextIndex("01");
+                        }}
+                        onMouseEnter={() => {
+                          setSwiperImageEnter(1);
+                        }}
+                        onMouseLeave={() => {
+                          setSwiperImageEnter(0);
                         }}
                       >
-                        <SwiperImage url={bg1} />
-                      </div>
-                      <div
+                        <IntroMainSwiperImage url={bg01} />
+                        <IntroMainSwiperImageOpacity
+                          style={
+                            swiperImageEnter === 1 ? { opacity: 0.5 } : null
+                          }
+                        />
+                      </IntroMainSwiperImageBox>
+                      <IntroMainSwiperImageBox
                         style={{
                           marginLeft: "60px",
                         }}
                         onClick={() => {
-                          setUnderBgIndex(bg2);
-                          console.log("2");
+                          setUnderBgIndex(bg02);
+                          setUpperTextIndex("02");
+                        }}
+                        onMouseEnter={() => {
+                          setSwiperImageEnter(2);
+                        }}
+                        onMouseLeave={() => {
+                          setSwiperImageEnter(0);
                         }}
                       >
-                        <SwiperImage url={bg2} />
-                      </div>
-                      <div
+                        <IntroMainSwiperImage url={bg02} />
+                        <IntroMainSwiperImageOpacity
+                          style={
+                            swiperImageEnter === 2 ? { opacity: 0.5 } : null
+                          }
+                        />
+                      </IntroMainSwiperImageBox>
+                      <IntroMainSwiperImageBox
                         style={{
                           marginLeft: "60px",
                         }}
                         onClick={() => {
-                          setUnderBgIndex(bg1);
-                          console.log("1");
+                          setUnderBgIndex(bg03);
+                          setUpperTextIndex("03");
+                        }}
+                        onMouseEnter={() => {
+                          setSwiperImageEnter(3);
+                        }}
+                        onMouseLeave={() => {
+                          setSwiperImageEnter(0);
                         }}
                       >
-                        <SwiperImage url={bg1} />
-                      </div>
-                      <div
+                        <IntroMainSwiperImage url={bg03} />
+                        <IntroMainSwiperImageOpacity
+                          style={
+                            swiperImageEnter === 3 ? { opacity: 0.5 } : null
+                          }
+                        />
+                      </IntroMainSwiperImageBox>
+                      <IntroMainSwiperImageBox
                         style={{
                           marginLeft: "60px",
                         }}
                         onClick={() => {
-                          setUnderBgIndex(bg2);
-                          console.log("2");
+                          setUnderBgIndex(bg04);
+                          setUpperTextIndex("04");
+                        }}
+                        onMouseEnter={() => {
+                          setSwiperImageEnter(4);
+                        }}
+                        onMouseLeave={() => {
+                          setSwiperImageEnter(0);
                         }}
                       >
-                        <SwiperImage url={bg2} />
-                      </div>
+                        <IntroMainSwiperImage url={bg04} />
+                        <IntroMainSwiperImageOpacity
+                          style={
+                            swiperImageEnter === 4 ? { opacity: 0.5 } : null
+                          }
+                        />
+                      </IntroMainSwiperImageBox>
                     </Swiper>
-                  </IntroMainSwiperBox>
+                  </IntroMainSwiperInner>
                 </IntroMainSwiperDiv>
               </IntroMainDiv>
             </IntroUpperRightDiv>
--- src/Styles/Theme.js
+++ src/Styles/Theme.js
@@ -1,4 +1,6 @@
 export default {
   bgDark: "#1F1F25",
   fontDark: "#FFFFFF",
+  grayDark: "#808080",
+  whiteDark: "#FFFFFF",
 };
