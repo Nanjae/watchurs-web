5eb80949211f57276b6e3363d22fd57007b6f242
Nanjae <33223057+Nanjae@users.noreply.github.com>
2020-05-18 19:21:06 +0900

    Create Loading component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,27 +1,62 @@
-import React from "react";
+import React, { Component } from "react";
 import GlobalStyles from "../Styles/GlobalStyles";
 import styled from "styled-components";
 import { BrowserRouter as Router } from "react-router-dom";
 import { DefaultRoute } from "./Routes";
 import "swiper/css/swiper.css";
+import { BlockLoading } from "react-loadingg";
 
 const Wrapper = styled.div`
   user-select: none;
 `;
 
-export default () => {
-  window.onbeforeunload = function () {
-    window.scrollTo(0, 0);
-  };
+const BlockLoadingDiv = styled.div`
+  z-index: 500;
+  position: fixed;
+  background-color: #1f1f25;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+  height: 50%;
+  opacity: 1;
+  transform: scale(2, 2) translate(25%, 25%);
+  transition: opacity 1s, z-index 1s, transform 1s;
+`;
 
-  return (
-    <>
-      <GlobalStyles />
-      <Router>
-        <Wrapper>
-          <DefaultRoute />
-        </Wrapper>
-      </Router>
-    </>
-  );
-};
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <>
+        <GlobalStyles />
+        <Router>
+          <Wrapper>
+            {this.state.isLoading ? (
+              <BlockLoadingDiv>
+                <BlockLoading size={"large"} />
+              </BlockLoadingDiv>
+            ) : (
+              <BlockLoadingDiv style={{ opacity: 0, zIndex: -50 }}>
+                <BlockLoading size={"large"} />
+              </BlockLoadingDiv>
+            )}
+            <DefaultRoute />
+          </Wrapper>
+        </Router>
+      </>
+    );
+  }
+}
