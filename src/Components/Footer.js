cdc8381255587d3309325fcafefd98c67988e001
Nanjae <33223057+Nanjae@users.noreply.github.com>
2020-06-03 19:03:36 +0900

    Update Footer component

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,6 +2,9 @@ import React from "react";
 import styled from "styled-components";
 import { useState } from "react";
 import ThemeDark from "../Styles/ThemeDark";
+import icon_clipboard_before from "../Assets/Icons/icon_clipboard_before.png";
+import icon_clipboard_after from "../Assets/Icons/icon_clipboard_after.png";
+import icon_clipboard_enter from "../Assets/Icons/icon_clipboard_enter.png";
 
 const Wrapper = styled.div`
   background-color: ${(props) => props.theme.bgFooterColor};
@@ -35,6 +38,15 @@ const Inner = styled.div`
   }
   @media only screen and (min-width: 1800px) {
   }
+`;
+
+const CommonIcon = styled.div`
+  background-image: url(${(props) => props.url});
+  background-size: cover;
+  background-position: center;
+  width: 18px;
+  height: 18px;
+  transition: background-image 0.3s;
 `;
 
 const TopDiv = styled.div`
@@ -131,6 +143,12 @@ const TopSubText = styled.div`
   transition: color 0.3s;
 `;
 
+const CopyEmailBox = styled.div`
+  height: 25%;
+  display: flex;
+  align-items: center;
+`;
+
 const BottomDiv = styled.div`
   width: 100%;
   height: 25%;
@@ -185,6 +203,12 @@ const BottomRightBox = styled.div`
 
 export default () => {
   const [textEnter, setTextEnter] = useState(0);
+  const [copied, setCopied] = useState(false);
+  const icon_clipboard = copied
+    ? icon_clipboard_after
+    : textEnter === 1
+    ? icon_clipboard_enter
+    : icon_clipboard_before;
 
   return (
     <>
@@ -198,21 +222,43 @@ export default () => {
               </TopMainTextBox>
               <TopSubTextBox>
                 <TopSubText>개발 : 난재(Nanjae)</TopSubText>
-                <TopSubText
+                <CopyEmailBox
                   onMouseEnter={() => {
                     setTextEnter(1);
                   }}
                   onMouseLeave={() => {
                     setTextEnter(0);
                   }}
-                  style={
-                    textEnter === 1
-                      ? { color: ThemeDark.highlightColor, cursor: "pointer" }
-                      : { cursor: "pointer" }
-                  }
+                  onClick={() => {
+                    if (!copied) {
+                      navigator.clipboard.writeText("dev.nanjae@gmail.com");
+                      setCopied(true);
+                      setTimeout(() => {
+                        setCopied(false);
+                      }, 2000);
+                    }
+                  }}
                 >
-                  dev.nanjae@gmail.com
-                </TopSubText>
+                  <TopSubText
+                    style={
+                      copied
+                        ? { color: "crimson", cursor: "auto" }
+                        : textEnter === 1
+                        ? { color: ThemeDark.highlightColor, cursor: "pointer" }
+                        : { cursor: "pointer" }
+                    }
+                  >
+                    {copied ? "복사완료" : "dev.nanjae@gmail.com"}
+                  </TopSubText>
+                  <CommonIcon
+                    url={icon_clipboard}
+                    style={
+                      copied
+                        ? { cursor: "auto", marginLeft: 5 }
+                        : { cursor: "pointer", marginLeft: 5 }
+                    }
+                  />
+                </CopyEmailBox>
                 <TopSubText
                   onClick={() => {
                     window.open(`https://watchurs.com`);
@@ -252,11 +298,14 @@ export default () => {
                           color: "crimson",
                           cursor: "pointer",
                           transition: "null",
+                          width: 166,
                         }
-                      : { cursor: "pointer", transition: "null" }
+                      : { cursor: "pointer", transition: "null", width: 166 }
                   }
                 >
-                  {textEnter === 2 ? "준비중입니다." : "리그 오브 레전드_LOL"}
+                  {textEnter === 2
+                    ? "준비 중입니다 (_ _)v"
+                    : "리그 오브 레전드_LOL"}
                 </TopSubText>
                 <TopSubText
                   onClick={() => {
